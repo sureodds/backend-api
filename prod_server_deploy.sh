f61d6e52fd2b3105b280a91dbfeb2417bba5c052
ikeogu <ikeogu322@gmail.com>
2023-08-05 02:05:13 +0100

    resolved deployment

--- prod_server_deploy.sh
+++ prod_server_deploy.sh
@@ -2,13 +2,14 @@
 set -e
 echo "Deploying application ..."
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
-
-    cd /var/www/sureodds-backend
-    # Update codebase
+ # Update codebase
     git pull origin main
     # Install dependencies based on lock file
     composer install --no-interaction --prefer-dist --optimize-autoloader --ignore-platform-reqs
+
+
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+
 
     # Migrate database
     php artisan migrate --force
