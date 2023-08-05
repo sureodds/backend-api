46f3cf195691d72b76ed19599f41fc01fb276d70
ikeogu <ikeogu322@gmail.com>
2023-08-05 01:50:02 +0100

    resolved deployment

--- prod_server_deploy.sh
+++ prod_server_deploy.sh
@@ -4,9 +4,10 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git pull origin master
+    git pull origin main
     # Install dependencies based on lock file
     composer install --no-interaction --prefer-dist --optimize-autoloader --ignore-platform-reqs
+    
     # Migrate database
     php artisan migrate --force
     # Note: If you're using queue workers, this is the place to restart them.
